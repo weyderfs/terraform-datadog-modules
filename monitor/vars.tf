86fbb934a6c395a649f1be20b7868eb41f62c366
Weyder <weyderfs@gmail.com>
2023-09-13 15:09:49 -0300

    Fix priority var validation condition

--- monitor/vars.tf
+++ monitor/vars.tf
@@ -34,7 +34,7 @@ variable "priority" {
   description = "(Number) Integer from 1 (high) to 5 (low) indicating alert severity."
 
   validation {
-    condition     = length(var.priority) <= 5
+    condition     = validate_integer(var.priority, 1, 5)
     error_message = "The max value is 5, where is it low"
   }
 }
